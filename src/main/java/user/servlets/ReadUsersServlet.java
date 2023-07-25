de945b0c92333ec897861863f485b7d08dcc12dd
Aryabhatt <bhattsahab314@gmail.com>
2023-07-25 23:35:19 +0530

    Updated ReadUsersServlet to use init param as per web.xml

--- src/main/java/user/servlets/ReadUsersServlet.java
+++ src/main/java/user/servlets/ReadUsersServlet.java
@@ -8,8 +8,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,54 +17,48 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ReadUsersServlet
  */
-@WebServlet("/ReadUsersServlet")
+//@WebServlet("/readServlet")
 public class ReadUsersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection;
- 
-	public void init() {
+
+	public void init(ServletConfig config) {
 		try {
 			System.out.println("ReadUsersServlet init()");
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root");
+			Class.forName(config.getInitParameter("SQLDriver"));
+			connection = DriverManager.getConnection(config.getInitParameter("dbUrl"),
+					config.getInitParameter("dbUser"), config.getInitParameter("dbPassword"));
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		try {
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery("select * from user");
 			PrintWriter out = response.getWriter();
-			out.print("<table border = '1' cellspacing = '8'>"
-					+ "<tr>"
-					+ 	"<th>Full Name</th>"
-					+ 	"<th>Email Address</th>"
-					+ "</tr>");
+			out.print("<table border = '1' cellspacing = '8'>" // table start
+					+ "<tr>" + "<th>Full Name</th>" + "<th>Email Address</th>" + "</tr>");
 			while (resultSet.next()) {
-				out.print("<tr>"
-						+ 	"<td>"
-						+ 		resultSet.getString(1) //firstName
-						+		"&nbsp"
-						+		resultSet.getString(2) //lastName
-						+	"</td>"
-						+	"<td>"
-						+		resultSet.getString(3) //email
-						+	"</td>"
-						+ "</tr>");
+				out.print("<tr>" + "<td>" + resultSet.getString(1) // firstName
+						+ "&nbsp" + resultSet.getString(2) // lastName
+						+ "</td>" + "<td>" + resultSet.getString(3) // email
+						+ "</td>" + "</tr>");
 			}
-			out.print("</table");
+			out.print("</table"); // table end
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void destroy() {
 		try {
 			connection.close();

d8e4b0fc618cf83a6b2d5f67a2bd947663607370
Aryabhatt <bhattsahab314@gmail.com>
2023-07-25 19:33:48 +0530

    Added a servlet to create/add user in dB

---
+++ src/main/java/user/servlets/CreateUserServlet.java
@@ -0,0 +1,79 @@
+package user.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateUserServlet
+ */
+@WebServlet("/addServlet")
+public class CreateUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+
+	public void init() {
+		try {
+			System.out.println("Create user init()");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root");
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// get all the parameters from the form
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		try {
+			// create statement object
+			Statement statement = connection.createStatement();
+			// varchar values need to be in single quotes
+			int result = statement.executeUpdate("insert into user values('" + firstName + "', '" + lastName + "', '"
+					+ email + "', '" + password + "')");
+			System.out.println(result + " row(s) got inserted.");
+
+			// writing back to browser
+			PrintWriter out = response.getWriter();
+			response.setContentType("text/html");
+
+			if (result > 0) {
+				out.println("<h2>USER CREATED</h2>");
+			} else {
+				out.println("<h2>Error creating the User</h2>");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}// end of doPost()
+
+	public void destroy() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}// end of CreateUserServlet class
